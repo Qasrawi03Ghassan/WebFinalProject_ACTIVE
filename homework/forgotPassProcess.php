e605d742ee0145c4790d7113afdb87eab35840e6
Qasrawi03Ghassan <gqasrawig@gmail.com>
2024-06-17 20:41:50 +0300

    Forgot password backend done (Only database now :D)

--- homework/forgotPassProcess.php
+++ homework/forgotPassProcess.php
@@ -1,9 +1,19 @@
 <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $sentEmail = $_POST["email"];
-        $afterAt = $sentEmail.str_split("@")[1];
-        if($afterAt.str_contains($afterAt,".com")){
+    if(isset($_POST['genCode'])) {
+        if (isset($_POST['email'])) {
+            $userEmail = $_POST['email'];
 
+            $to = $userEmail;
+            $subject = "New password recovery code request - Najah restaurant";
+            $message = "<html><body style='font-size: 20px;'>Your requested code is: <b style='font-size: 30px'>" . $_POST['genCode'] . "</b><br><br>Please make sure that you don't give it to anyone.</body></html>\n";
+            $headers = "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+            $headers .= "From: Najah restaurant - WebFinalProject";
+
+            if (mail($to, $subject, $message, $headers)) {
+                echo "email was sent successfully!\n";
+            } else {
+                echo "email sending failed!\n";
+            }
         }
     }
-
