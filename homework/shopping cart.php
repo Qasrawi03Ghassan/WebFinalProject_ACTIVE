c22cf828d766ad2e6195b2097e3afd59663a7409
Adel Qadi <Msys@DESKTOP-O3P379O>
2024-06-23 23:01:58 +0300

    I refuse to write a meaningful comment

--- homework/shopping cart.php
+++ homework/shopping cart.php
@@ -36,6 +36,7 @@
         let dishcounter =0;
         let Fprice=0;
         let checkit=false;
+        let notfound=false;
 
         function finalprice()
         {
@@ -76,6 +77,39 @@
                 finalprice();
         }
 
+        function deletedish(name)
+        {
+            document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+            document.getElementById('postform').submit();
+
+        }
+
+        function buttonhover(id){
+            document.getElementById(id).style.backgroundColor='#fbb114';
+        }
+
+        function buttonunhover(id){
+            document.getElementById(id).style.backgroundColor='orange';
+        }
+
+        function confirmpayment()
+        {
+            document.cookie.split(';').forEach(cookie => {
+                const eqPos = cookie.indexOf('=');
+                const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+                document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+            });
+            if(notfound)
+            {
+                alert('your order is empty!');
+            }
+            else {
+                alert('your order has been sent!');
+            }
+            document.getElementById('postform').submit();
+
+        }
+
 
 
     </script>
@@ -85,7 +119,8 @@
 
 
 <section>
-    <div style="font-size: 180%; position:absolute; top:3%; left: 5%; color: white" >Ordered dishes:</div>
+    <form id="postform" method="post"></form>
+    <div style="font-size: 180%; position:absolute; top:3%; left: 5%; color: white;" >Ordered dishes:</div>
 
     <div style="position: absolute; top: 7.5%; left: 5%; background-color: #23252e; width: 55%; height: 40%; overflow-y: auto; border-radius: 25px">
         <?php
@@ -120,9 +155,12 @@
         $id1 ='id';
         $id2='id';
         $num=0;
+        $color ='"'.'#fbb114'.'"';
+        $color2='"'.'orange'.'"';
 
 
-        echo "<table style='width: 100%; position: absolute; height: 200%; align-content: flex-start; border-spacing: 30px;'> </table> 
+
+        echo "<table id='dishtable' style='width: 100%; position: absolute; height: 200%; align-content: flex-start; border-spacing: 30px;'> </table> 
                 <tr style='max-height: 10px;'  > <span style='color: #23252e; '> filling space </span> </tr>";
         if(empty($dishes))
         {
@@ -131,11 +169,14 @@
         else {
             foreach ($dishes as $x) {
 
+
                 if (isset($_COOKIE[$x->name])) {
 
                     $dishesfound=true;
+                  echo "<script>notfound=false;</script>";
 
                     $name= $x->name;
+                    $name2= '"'.$x->name.'"';
 
                     $amount=$_COOKIE[$name];
 
@@ -149,8 +190,9 @@
              <span style='position: absolute; top: 40%; width 200px%; left: 150px; font-size: 130%;  '> $x->name</span>
              <input type='text' style='position: absolute; width: 35%; left: 230px; top:31px; ;height: 32px; border-radius: 10px; ' placeholder='comment any request/change' >
             
-              <button id=$id1 title='price' name=$x->price style=' background-color: orange; border-radius: 10px; border: 2px solid #d9640b; position: absolute; top:30px; left:70%; width: 150px; height: 40px; color: white; font-size: 120%;'><b> $x->price  </b>$</button>
-              <input id=$id2 oninput='alt_price($num)' type='number' step='1'  min='1' placeholder= $amount style='position: absolute; width: 30px; height: 33px;Top:30px; left:92%;border-radius: 5px; text-align: center' >
+              <button onmouseover='  document.getElementById(id).style.backgroundColor= $color;' onmouseleave='document.getElementById(id).style.backgroundColor= $color2;'  id=$id1 title='price' name=$x->price style=' background-color: orange; border-radius: 10px; border: 2px solid #d9640b; position: absolute; top:30px; left:70%; width: 120px; height: 40px; color: white; font-size: 120%;'><b> $x->price  </b>$</button>
+              <input id=$id2 oninput='alt_price($num)' type='number' step='1'  min='1' placeholder= $amount style='position: absolute; width: 30px; height: 33px;Top:30px; left:87%;border-radius: 5px; text-align: center' >
+              <button id=$id1 title='price' name=$x->price style=' border-radius: 100%; text-align: center;position: absolute; top:30px; left:94%; width: 40px; background-color: #1b1c24;height: 40px; color: orange; font-size: 120%;' onclick='deletedish($name2);'><b> X </b></button>
             </div>
             <button id=$id11 name='3' style='visibility: hidden'> </button>
             <script> alt_price($num);</script>
@@ -168,7 +210,53 @@
             if(!$dishesfound)
             {
                 echo" <div style='font-size: 150%; position:absolute; top:5%; left: 5%; color: white' >no dishes ordered.</div>";
+                echo "<script> notfound=true;</script>";
             }
+
+            if($num<=2)
+            {
+                echo" <script>
+              document.getElementById('dishtable').style.height='20%';
+              </script>
+            ";
+            }
+            if($num<=3)
+            {
+                echo" <script>
+              document.getElementById('dishtable').style.height='25%';
+              </script>
+            ";
+            }
+            if($num<=4)
+            {
+                echo" <script>
+              document.getElementById('dishtable').style.height='30%';
+              </script>
+            ";
+            }
+            if($num<=5)
+            {
+                echo" <script>
+              document.getElementById('dishtable').style.height='35%';
+              </script>
+            ";
+            }
+            if($num<=6)
+            {
+                echo" <script>
+              document.getElementById('dishtable').style.height='40%';
+              </script>
+            ";
+            }
+
+            if($num >6)
+            {
+                echo" <script>
+              document.getElementById('dishtable').style.height='60%';
+              </script>
+            ";
+            }
+
         }
 
 
@@ -203,10 +291,10 @@
         </div>
         <div class="inputname" style ="top:45%" >
             <span style=" position: absolute; width:50%; left: 25%"> CVV:</span>
-            <input id="cvv"   placeholder="  123/YY" style="width: 45%; height: 70%; left: 45%; position: absolute" >
+            <input id="cvv"   placeholder="  123" style="width: 45%; height: 70%; left: 45%; position: absolute" >
         </div>
 
-        <button title='price' style=' background-color: orange; border-radius: 20px; border: 2px solid #d9640b; position: absolute; top:70%; left:15%; width: 70%; height: 25%; color: white; font-size: 120%;' > <b>Confirm order<b> </b></button>
+        <button id="paybutton" onmouseover="buttonhover('paybutton')" onmouseleave="buttonunhover('paybutton')" onclick="confirmpayment()" title='price' style=' background-color: orange; border-radius: 20px; border: 2px solid #d9640b; position: absolute; top:70%; left:15%; width: 70%; height: 25%; color: white; font-size: 120%;' > <b>Confirm order<b> </b></button>
 
         <div id = 'totalprice' style="font-size: 150%; position:absolute; top:60%; left: 0%; color: white; width: 100%; text-align: center" >Total price: 100 $ </div>
 
